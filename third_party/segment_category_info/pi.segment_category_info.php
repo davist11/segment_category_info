4ebad819cf397e938629405659ef69b42618a922
Trevor Davis <davist11@gmail.com>
2013-01-14 09:37:53 -0500

    fix bug when multiple category groups are assigned to a channel

--- third_party/segment_category_info/pi.segment_category_info.php
+++ third_party/segment_category_info/pi.segment_category_info.php
@@ -26,7 +26,7 @@
 
 $plugin_info = array(
 	'pi_name'		=> 'Segment Category Info',
-	'pi_version'	=> '1.2',
+	'pi_version'	=> '1.3',
 	'pi_author'		=> 'Trevor Davis',
 	'pi_author_url'	=> 'http://trevordavis.net/',
 	'pi_description'=> 'Return category info by passing in the category_url_title and channel_short_name.',
@@ -49,30 +49,47 @@ class Segment_category_info {
 		$channel = $this->EE->TMPL->fetch_param('channel', NULL);
 		
 		if($segment && $channel) {
-			$query = $this->EE->db->select('cat_id, cat_name, cat_description, cat_image')
-					->from('channels')
-					->where('channel_name', $channel)
-					->where('cat_url_title', $segment)
-					->join('categories', 'channels.cat_group = categories.group_id')
-					->limit(1)
-					->get();
-			
-			foreach ($query->result() AS $row) {
-				$tagdata = $this->EE->TMPL->tagdata;
-				
-				//Parse file paths
-				$this->EE->load->library('typography');
-				$this->EE->typography->parse_images = TRUE;
-				$cat_image = $this->EE->typography->parse_file_paths($row->cat_image);
+			//Get the category groups on the channel
+			$query = $this->EE->db->select('cat_group')
+								->from('channels')
+								->where('channel_name', $channel)
+								->limit(1)
+								->get();
 
-				$variables[] = array(
-									'category_id' => $row->cat_id,
-									'category_name' => $row->cat_name,
-									'category_description' => $row->cat_description,
-									'category_image' => $cat_image
-									);
+			if ($query->num_rows() > 0) {
+				$row = $query->row_array();
+				$query->free_result();
 
-				$this->return_data = $this->EE->TMPL->parse_variables($tagdata, $variables);
+				$cat_groups = explode('|', $row['cat_group']);
+
+				//Get the category info
+				$query = $this->EE->db->select('cat_id, cat_name, cat_description, cat_image')
+									->from('categories')
+									->where('cat_url_title', $segment)
+									->where_in('group_id', $cat_groups)
+									->limit(1)
+									->get();
+
+				if ($query->num_rows() > 0) {
+					$row = $query->row_array();
+					$query->free_result();
+
+					$tagdata = $this->EE->TMPL->tagdata;
+					
+					//Parse file paths
+					$this->EE->load->library('typography');
+					$this->EE->typography->parse_images = TRUE;
+					$cat_image = $this->EE->typography->parse_file_paths($row['cat_image']);
+
+					$variables[] = array(
+										'category_id' => $row['cat_id'],
+										'category_name' => $row['cat_name'],
+										'category_description' => $row['cat_description'],
+										'category_image' => $cat_image
+										);
+
+					$this->return_data = $this->EE->TMPL->parse_variables($tagdata, $variables);
+				}
 			}
 		}
 	}
